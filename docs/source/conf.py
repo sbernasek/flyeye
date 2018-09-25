90848e7815d96d4600f55118eece2656341e6e11
Sebastian Bernasek <sebastian@u.northwestern.edu>
2018-09-25 16:31:28 -0500

    built docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,7 +96,7 @@ html_theme_options = {
 
     # sidebar
     'sidebar_collapse': True,
-    'sidebar_width': '150px',
+    'sidebar_width': '200px',
 
     # powered by note
     'show_powered_by': False,
