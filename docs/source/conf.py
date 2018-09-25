2a0c931d17f78f92545f1a37e66f7977ce171ae9
Sebastian Bernasek <sebastian@u.northwestern.edu>
2018-09-25 15:54:13 -0500

    built docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,7 @@ html_theme = 'alabaster'
 html_theme_options = {
 
     # logo
-    'logo': 'img/logo-01.png',
+    'logo': 'img/analysis_logo.png',
     #'logo_name': False,
     #'logo_text_align': 'center',
 
